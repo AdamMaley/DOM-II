a8c06b25ced854c6f3c57417461598bab862dd84
AdamMaley <adamemaley@yahoo.com>
2019-12-06 18:38:47 -0500

    initial commit, first one done.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+const logoMouseOver = document.querySelector(".logo-heading");
+logoMouseOver.addEventListener("mouseover", (e) => {
+    e.target.style.backgroundColor = "green";
+});
+
+const fKey = document.querySelector("body");
